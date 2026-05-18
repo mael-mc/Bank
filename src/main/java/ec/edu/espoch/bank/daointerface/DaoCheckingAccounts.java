8920868add2e937da1ec0ca916076d5b0483f777
mael-mc <juankmoreno4@gmail.com>
2026-05-18 18:29:32 -0500

    BANK

--- src/main/java/ec/edu/espoch/bank/daointerface/DaoCheckingAccounts.java
+++ src/main/java/ec/edu/espoch/bank/daointerface/DaoCheckingAccounts.java
@@ -1,11 +1,16 @@
 
 package ec.edu.espoch.bank.daointerface;
 
+import ec.edu.espoch.bank.objects.CheckingAccount;
+
 
 public interface DaoCheckingAccounts {
     
-    public boolean deposit(float value);
+    public boolean deposit(CheckingAccount checkingAccount, float value);
+    
     public boolean withdraw(float value);
+    
     public boolean monthlyStatement();
+    
     public void print();
 }
--- src/main/java/ec/edu/espoch/bank/datasource/CheckingAccounts.java
+++ src/main/java/ec/edu/espoch/bank/datasource/CheckingAccounts.java
@@ -1,13 +1,18 @@
 
 package ec.edu.espoch.bank.datasource;
 
-import ec.edu.espoch.bank.daointerface.DaoSavingAccounts;
+import ec.edu.espoch.bank.daointerface.DaoCheckingAccounts;
+import ec.edu.espoch.bank.objects.CheckingAccount;
 
-public class CheckingAccounts extends Accounts implements DaoSavingAccounts{
+public class CheckingAccounts extends Accounts implements DaoCheckingAccounts {
     
     @Override
-    public boolean deposit(float value){
-        return true;
+    public boolean deposit( CheckingAccount checkingAccount , float value){
+        boolean rsp= false;
+        if(value>0){
+            rsp = super.deposit(checkingAccount, value);
+        }
+        return rsp;
     }
     
     @Override
